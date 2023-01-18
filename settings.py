a233a4db11d764ebc870243ea510f9cfe148e4e4
Ma3ypuk <p.a.mazurik@mail.ru>
2023-01-19 00:12:26 +0300

    m28

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = "E:\Chrome_driver\chromedriver.exe"
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'p.a.mazurik@mail.ru'
+    valid_password = '328362'
+    valid_email_for_reg = 'p.a.mazurik@mail.ru'
+    invalid_email = 'pamazurik@mail.ru'
+    invalid_password = '333333'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Василий'
+    l_name = 'Васильев'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'и'
+    latin_generate_string = 'ssttww'
+    chinese_chars = '我不会说汉语请做不辣的怎么样你会说英语'
+    special_chars = '!@#$$%^&*()_+_+=//{}'
+    numbers = 987
+    empty = ''
+    passw1 = '123qWe'
+    passw2 = '123qweRTY456'
+    passw3 = '1234QwErTy567890uIoP'
